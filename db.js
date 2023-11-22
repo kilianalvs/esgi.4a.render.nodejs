fcf81df3e9cee88b1086f3dd1d0b0e84862373be
kilian_alvs <kilian.alves82@gmail.com>
2023-11-22 16:12:00 +0100

    modif bdd 2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  'postgres://tp_postgresql_cloud_user:DR2BlMHIjMhE7JwYUjxT6UaSdu72JPVx@dpg-clf1cq415k1s73f7cdh0-a/tp_postgresql_cloudpostgres://tp_postgresql_cloud_user:DR2BlMHIjMhE7JwYUjxT6UaSdu72JPVx@dpg-clf1cq415k1s73f7cdh0-a/tp_postgresql_cloud', // TODO: database connection string
+  process.env.URL_BDD, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
